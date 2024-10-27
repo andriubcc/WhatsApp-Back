47e45b71e67262c26041c5ed4b58b2df31e30c42
Andriu Barcellos <andriubcc@gmail.com>
2024-10-27 16:48:21 -0300

    add

--- src/server.js
+++ src/server.js
@@ -1,28 +1,64 @@
 const express = require('express');
 const http = require('http');
-const soquetIo = require('socket.io');
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = soquetIo(server);
+const io = socketIo(server);
 
 const port = process.env.PORT || 4000;
 
 const users = [];
+const rooms = {};
 
 io.on('connection', (socket) => {
-    socket.on("disconnect", () => {
-
-    })
-    socket.on("join", (name) => {
-        const user = {id: socket.id, name};
+    socket.on("join", (name, roomName) => {
+        const user = {id: socket.id, name, roomName};
         users.push(user);
-        io.emit("users", users)
-    })
 
-    socket.on("message", (message) => {
-        io.emit("message",message);
-    })
+        socket.join(roomName);
+
+        if (!rooms[roomName]) {
+            rooms[roomName] = [];
+        }
+        rooms[roomName].push(name);
+
+
+        io.emit("users", users)
+        io.emit("room", Object.keys(rooms));
+        console.log(`${name} entrou na ${roomName}`)
+    });
+
+    socket.on("leave", (roomName) => {
+        socket.leave(roomName);
+        console.log(`${socket.id} saiu da sala ${roomName}`);
+    });
+    
+    socket.on("message", (messageData) => {
+        io.to(messageData.roomName).emit("message", messageData);
+    });
+
+    socket.on("disconnect", () => {
+        const index = users.findIndex(user => user.id === socket.id);
+        if (index !== -1) {
+            const user = users.splice(index, 1)[0];
+
+
+            if (rooms[user.roomName]) {
+                rooms[user.roomName] = rooms[user.roomName].filter(u => u !== user.name);
+                
+
+                if (rooms[user.roomName].length === 0) {
+                    delete rooms[user.roomName];
+                }
+            }
+
+            io.emit("users", users);
+            io.emit("room", Object.keys(rooms));
+
+            console.log(`${user.name} saiu da sala ${user.roomName}`);
+        }
+    });
 })
 
 server.listen(port, () => console.log(`servidor rodando na porta ${port}`))
